de5845980bea64197b5ce879c6551cb15ef1b13a
Thiago kauai <html5ephp@gmail.com>
2020-01-14 12:04:32 -0300

    criando searchController implementando a busca de devs

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,10 +1,24 @@
-const Dev = require('./DevController')
+const Dev = require('../models/Dev')
 const parseStringAsArray = require('./utils/parseStringAsArray')
 
 module.exports = {
     async index(req,res){
         const { latitude, longitude, techs } = req.query
         const techsArray =  parseStringAsArray(techs)
-        res.json(techsArray)
+        const devs = await Dev.find({
+            techs:{
+                $in:techsArray
+            },
+            location:{
+                $near:{
+                    $geometry:{
+                        type:'Point',
+                        coordinates:[ longitude, latitude ] 
+                    },
+                    $maxDistance:10000
+                }
+            }
+        })
+        res.json({ devs })
     }
 }
