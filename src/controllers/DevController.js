3468a61e48bca14a1eb4e555324f8c06919bbd71
Thiago kauai <html5ephp@gmail.com>
2020-01-14 11:17:47 -0300

    impedindo dados repetidos no banco

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -3,23 +3,27 @@ const Dev = require('../models/Dev')
 
 module.exports = {
     async store(req, res) {
-        const { github_username,techs,latitude,longitude } = req.body
-        const { name = login, avatar_url, bio } = (await axios.get(`https://api.github.com/users/${github_username}`)).data
-        console.log(`${name},${bio},${avatar_url}`)
+        const { github_username, techs, latitude, longitude } = req.body
+        let dev = await Dev.findOne({ github_username }) 
         
-        const techsArray = techs.split(',').map(item => item.trim())
-        const location = {
-            type:'Point',
-            coordinates:[longitude,latitude]
+        if(!dev) {
+            const { name = login, avatar_url, bio } = (await axios.get(`https://api.github.com/users/${github_username}`)).data
+            const techsArray = techs.split(',').map(item => item.trim())
+            const location = {
+                type:'Point',
+                coordinates:[longitude,latitude]
+            }
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs:techsArray,
+                location
+            })
+
         }
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs:techsArray,
-            location
-        })
+
         return res.json(dev)
     }
 }
